54501219a65f42f783a86c4d8cc8c2ce249cd932
Caleb Riddlesperger <127689960+Riddlcal@users.noreply.github.com>
2024-04-28 16:32:06 -0500

    Update addVehicle.js

--- src/pages/api/addVehicle.js
+++ src/pages/api/addVehicle.js
@@ -1,12 +1,23 @@
 import { query } from "@/lib/db";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { manufacturerId, name, year, engineType, fuelType, mileage } = req.body;
+  if (req.method === 'GET') {
+    try {
+      const products = await query({
+        query: "SELECT * FROM Inventory",
+        values: [],
+      });
+      res.status(200).json({ products: products });
+    } catch (error) {
+      console.error('Error fetching products:', error.message);
+      res.status(500).json({ error: 'Failed to fetch products. Please try again.' });
+    }
+  } else if (req.method === 'POST') {
+    const { Car_ID, Manufacturer_ID, Car_Name, Year, Engine_Type, Fuel_Type, Mileage } = req.body;
     try {
       await query({
-        query: 'INSERT INTO Inventory (ManufacturerID, Name, Year, EngineType, FuelType, Mileage) VALUES (?, ?, ?, ?, ?, ?)',
-        values: [manufacturerId, name, year, engineType, fuelType, mileage],
+        query: 'INSERT INTO Inventory (Car_ID, Manufacturer_ID, Car_Name, Year, Engine_Type, Fuel_Type, Mileage) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        values: [Car_ID, Manufacturer_ID, Car_Name, Year, Engine_Type, Fuel_Type, Mileage],
       });
       res.status(200).json({ message: 'Vehicle added successfully!' });
     } catch (error) {
